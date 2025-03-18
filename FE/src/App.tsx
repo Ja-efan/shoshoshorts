159dbecd0a907cf2122dfb30d837c78791f5c82b
Dahyun Kim <tbvjekgus@gmail.com>
2025-03-18 13:19:19 +0900

    feat: Add nav and routes in App.tsx, connect NotFound page #S12P21B106-12

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,8 +1,20 @@
+import { Routes, Route, Link } from "react-router-dom";
+
+import NotFound from "./pages/notFound";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline"> world!</h1>
-    </>
+    <div className="p-4">
+      hello world
+      <nav className="flex gap-4 mb-4">
+        <Link to="/" className="text-blue-500">Home</Link>
+        <Link to="/about" className="text-blue-500">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
 
