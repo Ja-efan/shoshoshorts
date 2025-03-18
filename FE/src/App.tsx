13b8041c8722f64ad904067e859ba9f9da56a5a0
Dahyun Kim <tbvjekgus@gmail.com>
2025-03-18 14:06:22 +0900

    feat: Add index.tsx in router folder for better routing maintenance and update App.tsx #S12P21B106-12

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,19 +1,11 @@
 import { Routes, Route, Link } from "react-router-dom";
 
 import NotFound from "./pages/notFound";
-
+import Router from "./router";
 function App() {
   return (
     <div className="p-4">
-      hello world
-      <nav className="flex gap-4 mb-4">
-        <Link to="/" className="text-blue-500">Home</Link>
-        <Link to="/about" className="text-blue-500">About</Link>
-      </nav>
-
-      <Routes>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Router />
     </div>
   );
 }
