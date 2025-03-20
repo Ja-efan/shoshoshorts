f7d7f3d09f1fcdfe4d8290673327fe7e750fd619
Yu Junghyun <a1017u@naver.com>
2025-03-20 12:53:42 +0900

    feat: 이미지 및 오디오 생성 기능 추가

--- BE/src/main/java/com/sss/backend/BackendApplication.java
+++ BE/src/main/java/com/sss/backend/BackendApplication.java
@@ -1,7 +1,10 @@
 package com.sss.backend;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,4 +12,13 @@ public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-} 
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        return objectMapper;
+    }
+
+
+}
