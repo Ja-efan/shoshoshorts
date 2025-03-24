62e5a072ce194d28657c18e609868b5c348f2392
geunhh <rmsgnl11@naver.com>
2025-03-24 19:51:03 +0900

    "feat:VideoStatusAllDTO 구현 #S12P21B106-83"

--- BE/src/main/java/com/sss/backend/api/dto/VideoStatusAllDTO.java
+++ BE/src/main/java/com/sss/backend/api/dto/VideoStatusAllDTO.java
@@ -1,2 +1,20 @@
-package com.sss.backend.api.dto;public class VideoStatusAllDTO {
+package com.sss.backend.api.dto;
+
+import com.sss.backend.domain.entity.Video;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VideoStatusAllDTO {
+    private String title;
+    private Video.VideoStatus status;
+    private String completedAt;
+    private String thumbnailUrl;
+    private String storyId;
+
 }
