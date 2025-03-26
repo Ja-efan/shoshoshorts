da77d95812fb7c1ba1a37b45a25bb60834bb41a8
Dahyun Kim <tbvjekgus@gmail.com>
2025-03-26 12:30:23 +0900

    feat: Add video_url to Video type #S12P21B106-50

--- FE/src/types/video.ts
+++ FE/src/types/video.ts
@@ -3,5 +3,6 @@ export interface VideoData {
   status: "FAILED" | "COMPLETED" | "PENDING"
   completed_at: string | null
   thumbnail_url: string | null
+  video_url: string | null
   story_id: string
 } 
