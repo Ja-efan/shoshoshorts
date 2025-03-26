514ea9c55e72c69910a63f75a0ad4d2cd21f098c
geunhh <rmsgnl11@naver.com>
2025-03-26 22:49:11 +0900

    feat:사용자 컨트롤러 추가 및 Security 설정에 OAuth2 서비스 적용 #S12P21B106-106

--- BE/src/main/java/com/sss/backend/config/Securityconfig.java
+++ BE/src/main/java/com/sss/backend/config/Securityconfig.java
@@ -1,10 +1,13 @@
 package com.sss.backend.config;
 
 
+import com.sss.backend.domain.service.CustomOauth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -18,12 +21,43 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class Securityconfig {
 
+    private final CustomOauth2UserService customOauth2UserService;
+
+    public Securityconfig(CustomOauth2UserService customOauth2UserService){
+        this.customOauth2UserService = customOauth2UserService;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors(withDefaults())  // corsConfigurationSource 빈을 자동으로 사용
                 .csrf(csrf -> csrf.disable())
         // 추가 security 설정
         ;
+        //From 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        //HTTP Basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        //oauth2
+        http
+//                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig
+                                .userService(customOauth2UserService))));
+
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated());
+
+        //세션 설정 : STATELESS
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
