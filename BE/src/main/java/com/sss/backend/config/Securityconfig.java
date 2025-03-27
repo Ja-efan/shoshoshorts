a02baf356d801e7a801b0a5c6f835ac62cc554c8
geunhh <rmsgnl11@naver.com>
2025-03-27 15:22:50 +0900

    feat:securityconfig-JWT및 Oauth 설정 추가#S12P21B106-106

--- BE/src/main/java/com/sss/backend/config/Securityconfig.java
+++ BE/src/main/java/com/sss/backend/config/Securityconfig.java
@@ -2,13 +2,16 @@ package com.sss.backend.config;
 
 
 import com.sss.backend.domain.service.CustomOauth2UserService;
+import com.sss.backend.jwt.JWTFilter;
+import com.sss.backend.oauth2.CustomSuccessHandler;
+import com.sss.backend.jwt.JWTUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -17,23 +20,29 @@ import java.util.Arrays;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configuration
-@EnableWebSecurity
+@Configuration // 설정 파일
+@EnableWebSecurity // Spring Security 활성화
 public class Securityconfig {
 
     private final CustomOauth2UserService customOauth2UserService;
+    private final CustomSuccessHandler customSuccessHandler;
+    private final JWTUtil jwtUtil;
 
-    public Securityconfig(CustomOauth2UserService customOauth2UserService){
+    public Securityconfig(CustomOauth2UserService customOauth2UserService, CustomSuccessHandler customSuccessHandler, JWTUtil jwtUtil) {
         this.customOauth2UserService = customOauth2UserService;
+        this.customSuccessHandler = customSuccessHandler;
+        this.jwtUtil = jwtUtil;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors(withDefaults())  // corsConfigurationSource 빈을 자동으로 사용
-                .csrf(csrf -> csrf.disable())
-        // 추가 security 설정
-        ;
-        //From 로그인 방식 disable
+
+        //CORS 설정
+        http
+                .cors(withDefaults())  // corsConfigurationSource 빈을 자동으로 사용
+                .csrf(csrf -> csrf.disable());
+
+        //From 로그인 방식 disable (OAuth2만 쓸거임)
         http
                 .formLogin((auth) -> auth.disable());
 
@@ -41,20 +50,26 @@ public class Securityconfig {
         http
                 .httpBasic((auth) -> auth.disable());
 
-        //oauth2
+        //JWTFilter 추가 : Spring Seucurity의 인증 필터 앞에 커스텀 JWT 필터르 끼워넣음
+        // 요청 헤더에서 JWT 토큰이 있는지 검사해서 사용자 인증 처리..
         http
-//                .oauth2Login(Customizer.withDefaults())
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        //oauth2 로그인 설정
+        http
                 .oauth2Login((oauth2) -> oauth2
                         .userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig
-                                .userService(customOauth2UserService))));
+                                .userService(customOauth2UserService)))
+                        .successHandler(customSuccessHandler)); //
 
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/").permitAll()
-                        .anyRequest().authenticated());
+                        .requestMatchers("/").permitAll() //루트 경로는 모두 접근 허용
+                        .requestMatchers("/admin/**").hasRole("ADMIN") // 관리자 페이지 설정 해주면 좋
+                        .anyRequest().authenticated());     // 그 외는 인증 필요함
 
-        //세션 설정 : STATELESS
+        //세션 설정 : STATELESS // 사용 안함
         http
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
@@ -65,7 +80,10 @@ public class Securityconfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173","http://shoshoshorts.duckdns.org"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173","http://shoshoshorts.duckdns.org",
+                "http://localhost:63342" // ← IntelliJ에서 HTML 열릴 때 이 주소로 열림
+                //Todo 개발 후 삭제
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
 //        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization", "X-Requested-With"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
@@ -79,3 +97,7 @@ public class Securityconfig {
 
 
 }
+/**
+ * @EnableWebSecurity 내의 Security Filter Chain 을 자동으로 찾아서 적용함.
+ *
+ */
