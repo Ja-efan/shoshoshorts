5982d2c9afd87dbfb1d5faa0336a6e27dc6faa60
Dahyun Kim <tbvjekgus@gmail.com>
2025-03-28 12:14:10 +0900

    feat:modifing router.tsx

--- FE/src/router/index.tsx
+++ FE/src/router/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { useRoutes } from "react-router-dom";
+import { useRoutes, createBrowserRouter } from "react-router-dom";
 import Layout from "../components/layout";
 import NotFound from "../pages/notFound";
-import LandingPage from "@/pages/LandingPage";
+import LandingPage from "@/pages/landingPage";
 import CreateVideoPage from "@/pages/createPage";
 import DashboardPage from "@/pages/dashboardPage";
+import LoginPage from "@/pages/loginPage";
+import TermsPage from "@/pages/terms";
+import { ProtectedRoute } from "@/components/common/ProtectedRoute";
+import AuthCallbackPage from "@/pages/auth/callback";
 
 const Router: React.FC = () => {
   return useRoutes([
@@ -12,13 +16,52 @@ const Router: React.FC = () => {
       path: "/",
       element: <Layout />,
       children: [
-        {path: '/', element: <LandingPage /> },
+        { path: '/', element: <LandingPage /> },
       ],
     },
-    {path: '/create', element: <CreateVideoPage/>},
-    {path: '/dashboard', element: <DashboardPage/>},
-    { path: "*", element: <NotFound /> }, 
+    { path: '/login', element: <LoginPage /> },
+    { path: '/terms', element: <TermsPage /> },
+    {
+      path: '/create',
+      element: (
+        <ProtectedRoute>
+          <CreateVideoPage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: '/dashboard',
+      element: (
+        <ProtectedRoute>
+          <DashboardPage />
+        </ProtectedRoute>
+      ),
+    },
+    { path: "*", element: <NotFound /> },
   ]);
 };
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ProtectedRoute><DashboardPage /></ProtectedRoute>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/create",
+    element: <ProtectedRoute><CreateVideoPage /></ProtectedRoute>,
+  },
+  {
+    path: "/terms",
+    element: <TermsPage />,
+  },
+  {
+    path: "/auth/:provider/callback",
+    element: <AuthCallbackPage />,
+  },
+]);
 
 export default Router;
