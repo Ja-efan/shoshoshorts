9e8aa5ffc4192e9bf2b105e939115dff587f9f31
geunhh <rmsgnl11@naver.com>
2025-03-29 20:35:30 +0900

    fix: USERDTO 생성자 추가, #S12P21B106-106

--- BE/src/main/java/com/sss/backend/api/dto/OAuth/UserDTO.java
+++ BE/src/main/java/com/sss/backend/api/dto/OAuth/UserDTO.java
@@ -1,12 +1,21 @@
 package com.sss.backend.api.dto.OAuth;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
     private String role;
     private String name;
     private String username;
+
+    public UserDTO(String username, String role) {
+        this.username = username;
+        this.role = role;
+    }
 }
