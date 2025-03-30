90aa3d83015897ac510ac49f9d7b985e7820d0d0
geunhh <rmsgnl11@naver.com>
2025-03-30 14:58:22 +0900

    chore:사용하지 않는 코드 제거 #S12P21B106-106

--- BE/src/main/java/com/sss/backend/jwt/JWTFilter.java
+++ BE/src/main/java/com/sss/backend/jwt/JWTFilter.java
@@ -27,22 +27,6 @@ public class JWTFilter extends OncePerRequestFilter {
         this.jwtUtil = jwtUtil;
     }
 
-//    private String resolveToken(HttpServletRequest request) {
-//        //1. Authorization 헤더에서 토큰 추출
-//        String bearerToken = request.getHeader("Authorization");
-//        if (bearerToken != null && bearerToken.startsWith("Bearer")) {
-//            return bearerToken.substring(7); // "Bearer " 제거
-//        }
-//        //2. 쿠키에서 Authorization 이름으로 찾기
-//        if (request.getCookies() != null) {
-//            for (Cookie cookie: request.getCookies()) {
-//                if ("Authorization".equals(cookie.getName())) {
-//                    return cookie.getName();
-//                }
-//            }
-//        }
-//        return null;
-//    }
     // 요청마다 실행되는 필터 로직
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -75,60 +59,5 @@ public class JWTFilter extends OncePerRequestFilter {
         }
         filterChain.doFilter(request,response);
 
-
-//        Cookie[] cookies = request.getCookies();
-//
-//        // 쿠키 중 "Authorization" 이름을 가진 쿠키 찾기 => JWT 추출.
-//        for (Cookie cookie : cookies) {
-//            if (cookie.getName().equals("Authorization")){
-//                authorization = cookie.getValue(); // JWT 토큰 저장
-//            }
-//        }
-//
-//        // Authorization 헤더 검증
-//        if (authorization == null){
-//            log.info("token null");
-//            filterChain.doFilter(request, response);
-//
-//            // 조건이 해당 되면 메소드 종료
-//            return;
-//        }
-//
-//        // 토큰 유효성 검사
-//        String token = authorization;
-//
-//        // 토큰 소멸 시간 검증
-//        if (jwtUtil.isExpired(token)) {
-//            log.info("token expired");
-//            filterChain.doFilter(request,response);
-//            return;
-//        }
-//
-//        // 토큰이 유효하면 >> username과 role (사용자 정보) 추출
-//        String username = jwtUtil.getUsername(token);
-//        String role = jwtUtil.getRole(token);
-//
-//        // userDTO 생성하여 값 세팅
-//        UserDTO userDTO = new UserDTO();
-//        userDTO.setUsername(username);
-//        userDTO.setRole(role);
-//
-//        // UserDTO 기반으로 CustomOAuth2User 생성 (UserDetails 구현체)
-//        CustomOAuth2User customOAuth2User = new CustomOAuth2User(userDTO);
-//
-//        // 스프링 시큐리티 인증 토큰(객체) 생성
-//        Authentication authToken = new UsernamePasswordAuthenticationToken(
-//                customOAuth2User,
-//                null,
-//                customOAuth2User.getAuthorities()
-//        );
-//        log.info("SecurityContextHolder에 저장될 사용자 정보 {}",customOAuth2User);
-//        log.info("SecurityContextHolder에 저장될 사용자 권한 {}",customOAuth2User.getAuthorities());
-//
-//        // 시큐리티 컨텍스트에 사용자 인증 정보 저장
-//        SecurityContextHolder.getContext().setAuthentication(authToken);
-//
-//        // 다음 필터로 요청 넘김
-//        filterChain.doFilter(request,response);
     }
 }
