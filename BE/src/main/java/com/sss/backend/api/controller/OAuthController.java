f80082aec876f2ca5de6d1383c8d1793ce10a451
geunhh <rmsgnl11@naver.com>
2025-04-01 14:03:33 +0900

    refact: Contoller & Service 구조 정리 #S12P21B106-134

--- BE/src/main/java/com/sss/backend/api/controller/OAuthController.java
+++ BE/src/main/java/com/sss/backend/api/controller/OAuthController.java
@@ -1,8 +1,6 @@
 package com.sss.backend.api.controller;
 
 import com.sss.backend.api.dto.OAuth.OAuthLoginRequest;
-import com.sss.backend.api.dto.TokenResponse;
-import com.sss.backend.domain.entity.UserEntity;
 import com.sss.backend.domain.repository.UserRepository;
 import com.sss.backend.domain.service.OAuthService;
 import com.sss.backend.domain.service.RedisService;
@@ -23,109 +21,42 @@ import java.util.Map;
 public class OAuthController {
 
     private final OAuthService oAuthService;
-    private final JWTUtil jwtUtil;
-    private final UserRepository userRepository;
     private final TokenService tokenService;
-    private final RedisService redisService;
 
     /**
      * OAuth 로그인 및 토큰 유효성 검사를 위한 Controller
      */
     @PostMapping("/oauth")
     public ResponseEntity<?> oauthLogin(@RequestBody OAuthLoginRequest request) {
-        log.info("post 요청, {}",request);
+        log.info("로그인/회원가입 요청, {}",request);
         return oAuthService.processOAuthLogin(request.getProvider(), request.getCode());
     }
 
     /**
      * accessToken 유효성 확인용 API
-     * @return
      */
     @GetMapping("/check")
     public ResponseEntity<?> checkTokenValidity(HttpServletRequest request){
-        String token = jwtUtil.extractTokenFromRequest(request);
-        log.info("token : {}",token);
-
-        // 토큰 유효성 검사
-        if (token == null || jwtUtil.isExpired(token) ) {
-            log.warn("유효하지 않은 토큰 또는 만료된 토큰");
-            // UNAUTHORIZED 반환.
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(Map.of(
-                            "valid",false,"message",
-                            "Invalid or expired token")
-                    );
-        }
-        log.info("유효한 토큰입니다.");
-        // TRUE 반환
-        return ResponseEntity.ok(Map.of("valid",true));
+        log.info("Access 토큰 유효성 검사 API 실행");
+        return tokenService.validiateToken(request);
     }
 
     /**
      * 로그아웃 메소드
      * refreshToken 제거 및 쿠키 제거
-     * @param request
-     * @return
      */
     @PostMapping("/logout")
     public ResponseEntity<?> logout(HttpServletRequest request){
-        // 쿠키에서 Refresh 토큰 조회
-        String refreshToken = jwtUtil.extractRefreshTokenFromCookie(request);
-
-        // Refresh 토큰 제거 : Redis에서 refresh:{email} 키 삭제
-        if (refreshToken != null || !jwtUtil.isExpired(refreshToken)) {
-            String email = jwtUtil.getEmail(refreshToken);
-            redisService.deleteToken(email);
-        }
-
-        // HttpOnly 쿠키 삭제 : 프론트에 Set-Cookie 응답 보내서 refresh 쿠키를 빈값으로 설정
-        ResponseCookie deleteCookie = ResponseCookie.from("refreshToken","")
-                .httpOnly(true) // JS에서 접근 불가
-                .secure(false)   // https일때만 쿠키 전송
-                .path("/")
-                .maxAge(60*60)
-                .sameSite("Lax")
-                .build();
-        // todo : 배포시 secure true로 바꿔주자.
-
-        return ResponseEntity.ok(Map.of("message","로그아웃 완료"));
+        log.info("logout API 실행");
+        return tokenService.logoutToken(request);
     }
 
 
     @PostMapping("/refresh")
     public ResponseEntity<?> refreshToken(HttpServletRequest request) {
         log.info("refresh 로직 On. {} ###",request);
+        return tokenService.refreshToken(request);
 
-//        String refreshToken = jwtUtil.extractTokenFromRequest(request);
-        String refreshToken = jwtUtil.extractRefreshTokenFromCookie(request);
-        log.info("refreshToken {}", refreshToken );
-
-        if (refreshToken == null || jwtUtil.isExpired(refreshToken)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body("RefreshToken이 null입니다. 유효하지 않습니다");
-        }
-
-        String email = jwtUtil.getEmail(refreshToken);
-
-        // Redis에 저장된 refresh 토큰과 비교
-        String savedRefresheToken = redisService.getToken("refresh:"+email);
-        if (savedRefresheToken == null || !savedRefresheToken.equals(refreshToken)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body("RefreshToken이 유효하지 않습니다.");
-        }
-
-        // 사용자 정보 조회
-        UserEntity user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("사용자 정보를 찾을 수 없음"));
-
-        // Access / Refresh Token 갱신
-        String accessToken = tokenService.createAccessToken(user);
-        String newRefreshToken = tokenService.createAndStoreRefreshToken(email);
-        ResponseCookie cookie = tokenService.createRefreshTokenCookie(newRefreshToken);
-
-        return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE, cookie.toString())
-                .body(new TokenResponse(accessToken));
     }
 
 }
