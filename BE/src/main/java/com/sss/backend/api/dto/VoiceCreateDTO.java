8b155ec055351dd54780c784cfbb397a8446cb54
geunhh <rmsgnl11@naver.com>
2025-04-06 17:03:35 +0900

    feat: VoiceCreate 요청 유효성검사 #S12P21B106-197

--- BE/src/main/java/com/sss/backend/api/dto/VoiceCreateDTO.java
+++ BE/src/main/java/com/sss/backend/api/dto/VoiceCreateDTO.java
@@ -1,13 +1,17 @@
 package com.sss.backend.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class VoiceCreateDTO {
+
+    @NotBlank(message = "보이스 이름은 필수입니다.")
     private String voice_name;
 
+    @NotBlank(message = "오디오 파일은 필수입니다.")
     private String audio_file; // base64 문자열
 
 
